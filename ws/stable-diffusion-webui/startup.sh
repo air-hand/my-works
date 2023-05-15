12440e126efb3981601313dc2ce9484187139211
air-hand <45233435+air-hand@users.noreply.github.com>
2023-05-16 01:28:35 +0900

    feat: add stable-diffusion-webui.

---
+++ ws/stable-diffusion-webui/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bash <(curl -fsSL https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh) -f
---
+++ ws/stable-diffusion-webui/webui-user.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#########################################################
+# Uncomment and change the variables below to your need:#
+#########################################################
+# original script at https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/webui-user.sh
+
+# Install directory without trailing slash
+#install_dir="/home/$(whoami)"
+install_dir="$(pwd)/cloned"
+
+# Name of the subdirectory
+#clone_dir="stable-diffusion-webui"
+
+# Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
+export COMMANDLINE_ARGS="--lowvram"
+
+# python3 executable
+#python_cmd="python3"
+
+# git executable
+#export GIT="git"
+
+# python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
+#venv_dir="venv"
+
+# script to launch to start the app
+#export LAUNCH_SCRIPT="launch.py"
+
+# install command for torch
+#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
+export TORCH_COMMAND="pip install --no-cache-dir torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
+
+# Requirements file to use for stable-diffusion-webui
+#export REQS_FILE="requirements_versions.txt"
+
+# Fixed git repos
+#export K_DIFFUSION_PACKAGE=""
+#export GFPGAN_PACKAGE=""
+
+# Fixed git commits
+#export STABLE_DIFFUSION_COMMIT_HASH=""
+#export TAMING_TRANSFORMERS_COMMIT_HASH=""
+#export CODEFORMER_COMMIT_HASH=""
+#export BLIP_COMMIT_HASH=""
+
+# Uncomment to enable accelerated launch
+export ACCELERATE="True"
+
+# Uncomment to disable TCMalloc
+#export NO_TCMALLOC="True"
+
+###########################################
