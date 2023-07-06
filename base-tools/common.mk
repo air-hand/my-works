aae1fa13467231c0b23543f7b970fa0d0a3bc977
air-hand <45233435+air-hand@users.noreply.github.com>
2023-07-07 01:50:51 +0900

    wip: init jenkins

--- base-tools/common.mk
+++ base-tools/common.mk
@@ -1,8 +1,13 @@
-DOCKERFILE_MK_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+COMMON_MK_DIR := $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
 
 ifndef MAKEFILE_DIR
 $(error "Must set MAKEFILE_DIR on includer makefile side.")
 endif
 
-Dockerfile: $(MAKEFILE_DIR)/Dockerfile.in $(DOCKERFILE_MK_DIR)/*
-	@cd $(MAKEFILE_DIR) && docker run --rm -it -v $(DOCKERFILE_MK_DIR):/base-tools -v $(MAKEFILE_DIR)/:/root:ro coryb/dfpp Dockerfile.in > Dockerfile
+Dockerfile: $(MAKEFILE_DIR)/Dockerfile.in $(COMMON_MK_DIR)/*
+	@cd $(MAKEFILE_DIR) && docker run --rm -it -v $(COMMON_MK_DIR):/base-tools -v $(MAKEFILE_DIR)/:/root:ro coryb/dfpp Dockerfile.in > Dockerfile
+
+.PHONY: init-devcontainer
+init-devcontainer:
+	@cd $(MAKEFILE_DIR) && \
+	PROJECTNAME=$$(basename $(MAKEFILE_DIR)) bash $(COMMON_MK_DIR)/initialize-devcontainer.sh
